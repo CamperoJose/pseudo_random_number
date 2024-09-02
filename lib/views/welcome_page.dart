dd1a9172ca546dc67cd860a4f1fbd4ecb8b7b901
CamperoJose <jose.campero@ucb.edu.bo>
2024-09-02 11:31:45 -0400

    update welcome page

--- lib/views/welcome_page.dart
+++ lib/views/welcome_page.dart
@@ -19,155 +19,209 @@ class WelcomePage extends StatelessWidget {
             Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: NetworkImage('https://res.cloudinary.com/deaodcmae/image/upload/v1724986931/jwfefm8l1qbswsdvx4va.png'),
+                  image: NetworkImage(
+                      'https://res.cloudinary.com/deaodcmae/image/upload/v1724986931/jwfefm8l1qbswsdvx4va.png'),
                   fit: BoxFit.cover,
                 ),
               ),
             ),
-            Column(
-              children: [
-                // Título centrado en la parte superior
-                const Padding(
-                  padding: EdgeInsets.only(top: 80.0, bottom: 20.0),
-                  child: Center(
-                    child: Text(
-                      'Generador de Números Pseudoaleatorios v0.0 CD',
-                      style: TextStyle(
-                        fontSize: 42.0,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.white,
-                        letterSpacing: 2.0,
-                        shadows: [
-                          Shadow(
-                            offset: Offset(3, 3),
-                            blurRadius: 8.0,
-                            color: Colors.black54,
-                          ),
-                          Shadow(
-                            offset: Offset(-3, -3),
-                            blurRadius: 8.0,
-                            color: Colors.black26,
-                          ),
-                        ],
-                      ),
-                      textAlign: TextAlign.center,
+            LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
                     ),
-                  ),
-                ),
-
-                Expanded(
-                  child: Center(
-                    child: Container(
-                      padding: EdgeInsets.all(25.0),
-                      margin: EdgeInsets.symmetric(horizontal: 30.0),
-                      width: double.infinity * 0.66,
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 200, 198, 198).withOpacity(0.2),
-                        borderRadius: BorderRadius.circular(25.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black38,
-                            blurRadius: 10,
-                            offset: Offset(0, 5),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SizedBox(height: 20.0),
-                          // Columnas de contenido con iconos
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Columna izquierda
-                              const Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      '¿Qué son los números pseudoaleatorios?',
-                                      style: TextStyle(
-                                        fontSize: 26.0,
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    SizedBox(height: 20.0),
-                                    Text(
-                                      'Los números pseudoaleatorios son secuencias de números que parecen ser aleatorios, pero en realidad son generados por un algoritmo determinista. Estos números se utilizan en simulaciones, criptografía y muchos otros campos.',
-                                      style: TextStyle(
-                                        fontSize: 18.0,
-                                        color: Colors.white70,
-                                        height: 1.5,
-                                      ),
-                                    ),
-                                  ],
-                                ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 80.0, bottom: 20.0),
+                          child: Center(
+                            child: Text(
+                              'Generador de Números Pseudoaleatorios v0.0 CD',
+                              style: TextStyle(
+                                fontSize: 42.0,
+                                fontWeight: FontWeight.w900,
+                                color: Colors.white,
+                                letterSpacing: 2.0,
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(3, 3),
+                                    blurRadius: 8.0,
+                                    color: Colors.black54,
+                                  ),
+                                  Shadow(
+                                    offset: Offset(-3, -3),
+                                    blurRadius: 8.0,
+                                    color: Colors.black26,
+                                  ),
+                                ],
                               ),
-                              const SizedBox(width: 40.0),
-                              // Columna derecha con botones animados
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    const Text(
-                                      'Escoja un método',
-                                      style: TextStyle(
-                                        fontSize: 26.0,
-                                        fontWeight: FontWeight.w600,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                    SizedBox(height: 20.0),
-                                    // Botones de métodos con animación de escala
-                                    AnimatedButton(
-                                      text: 'Cuadrados Medios',
-                                      onPressed: () {
-                                        //ir a la página del método 1:
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => MethodOnePage()),
-                                        );
-                                      },
-                                    ),
-                                    SizedBox(height: 10.0),
-                                    AnimatedButton(
-                                      text: 'Productos Minimos',
-                                      onPressed: () {
-                                        Navigator.push(
-                                          context,
-                                          MaterialPageRoute(builder: (context) => MethodTwoPage()),
-                                        );
-                                      },
-                                    ),
-                                    SizedBox(height: 10.0),
-                                    Text("metodos congruenciales"),
-                                    AnimatedButton(
-                                      text: 'Algorítmo Congruencial Lineal',
-                                      onPressed: () {
-                                        Navigator.push(context, 
-                                        
-                                        MaterialPageRoute(builder: (context) => MethodThreePage()),
-                                        );
-                                      },
-                                    ),
-                                  ],
-                                ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                          padding: EdgeInsets.all(25.0),
+                          margin: EdgeInsets.symmetric(horizontal: 30.0),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 200, 198, 198)
+                                .withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(25.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black38,
+                                blurRadius: 10,
+                                offset: Offset(0, 5),
                               ),
                             ],
                           ),
-                        ],
-                      ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 20.0),
+                              // Sección de información adicional
+
+
+                              // Columnas de contenido con iconos
+                              if (constraints.maxWidth > 600)
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Expanded(
+                                      child: _buildInfoColumn(),
+                                    ),
+                                    const SizedBox(width: 40.0),
+                                    Expanded(
+                                      child: _buildMethodsColumn(context),
+                                    ),
+                                  ],
+                                )
+                              else
+                                Column(
+                                  children: [
+                                    _buildInfoColumn(),
+                                    const SizedBox(height: 20.0),
+                                    _buildMethodsColumn(context),
+                                  ],
+                                ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 20.0),
+                      ],
                     ),
                   ),
-                ),
-              ],
+                );
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildInfoColumn() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          '¿Qué son los números pseudoaleatorios?',
+          style: TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        const Text(
+          'Los números pseudoaleatorios son secuencias de números que parecen ser aleatorios, pero en realidad son generados por un algoritmo determinista. Estos números se utilizan en simulaciones, criptografía y muchos otros campos. En este proyecto, exploraremos cómo se generan por cuatro diferentes métodos.',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.white70,
+            height: 1.5,
+          ),
+          textAlign: TextAlign.justify,
+        ),
+        const SizedBox(height: 20.0),
+        const Text(
+          '¿Por qué son importantes?',
+          style: TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        const Text(
+          'La importancia de los números pseudoaleatorios radica en su capacidad para emular la aleatoriedad verdadera en aplicaciones prácticas donde no es posible utilizar fuentes de entropía pura. Esto es crucial en el diseño de sistemas criptográficos, simulaciones y pruebas aleatorias en ciencias computacionales.',
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.white70,
+            height: 1.5,
+          ),
+          textAlign: TextAlign.justify,
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMethodsColumn(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Text(
+          'Escoja un método',
+          style: TextStyle(
+            fontSize: 26.0,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 20.0),
+        // Botones de métodos con animación de escala
+        AnimatedButton(
+          text: 'Cuadrados Medios',
+          onPressed: () {
+            //ir a la página del método 1:
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MethodOnePage()),
+            );
+          },
+        ),
+        const SizedBox(height: 10.0),
+        AnimatedButton(
+          text: 'Productos Mínimos',
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MethodTwoPage()),
+            );
+          },
+        ),
+        const SizedBox(height: 10.0),
+        AnimatedButton(
+          text: 'Algoritmo Congruencial Lineal',
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MethodThreePage()),
+            );
+          },
+        ),
+        const SizedBox(height: 10.0),
+        AnimatedButton(
+          text: 'Algoritmo Congruencial Multiplicativo',
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MethodThreePage()),
+            );
+          },
+        ),
+      ],
     );
   }
 }
